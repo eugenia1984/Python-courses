f2311e099808644600e17661f749aa15d653f10d
Eugenia <costamariaeugenia1@gmail.com>
2021-02-07 01:24:28 -0300

    add files via upload
    Good practices

---
+++ 09-buenas-practicas.py
@@ -0,0 +1,19 @@
+"""
+COMENTAR TU CÓDIGO:
+*Ventajas:
+- Servir como recordatorio para el futuro y como documentación
+- Sno se debe comentar hasta lo más simple, comenta lo que pueda ser importante para el futuro.
+
+# Comentario de una sola línea
+
+Todo este cometario es ejempl ode comentario, en más de una línea, se marca entre tres comillas dobles
+"""
+
+"""
+ESPACIADO Y ORDEN:
+- Todo alineado
+- Cada cloque separado por un espacio
+- Solo identar dentro de las funciones
+- No dejar mucho espacio en blanco
+- Siempre tené un código ordenado, fácil de leer
+"""
