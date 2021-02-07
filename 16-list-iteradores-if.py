504b30c4fb171bc78498e9e7504523a956369b4f
Eugenia <costamariaeugenia1@gmail.com>
2021-02-07 15:40:45 -0300

    add files via upload
    Example with LIST, ITERATORS and IF

---
+++ 16-list-iteradores-if.py
@@ -0,0 +1,10 @@
+# Ejemplo con LIST, ITERADORES e IF
+
+lenguajes = ['Python', 'Kotlink', 'Java', 'JavaScript']
+
+for lenguaje in lenguajes:
+    if lenguaje == 'Python':
+        print(lenguaje.upper())
+    else: 
+        print(lenguaje)
+        
