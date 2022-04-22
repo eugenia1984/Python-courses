27832c2d95884f06e523f7a989cff799d390d301
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2022-04-22 10:38:13 -0300

    sigo con herencia

--- 01_curso_completo_de_python/dia08_herencia.py
+++ 01_curso_completo_de_python/dia08_herencia.py
@@ -1,3 +1,4 @@
+# Creo la clase Persona
 class Persona:
   def __init__(self, nombre, edad, apellido, sexo): # es el constructor
     self.nombre = nombre
@@ -11,11 +12,34 @@ class Persona:
     print(f'El apellido de la persona es: {self.apellido}')
     print(f'El sexo de la persona es: {self.sexo}')
 
+## Creo la clase Empleado que hereda de persona
+class Empleado(Persona):
+
+  def datosEmpleado(self, vacaciones, salario):
+    print(f'Sus dias de vacaciones son: {vacaciones}')
+    print(f'Su salario es: {salario}')
+
 miPersona = Persona('Raul', 22, 'Gonzalez', 'Masculino') # instancio un objeto de la clase persona
 miPersona.datosPersonales()
+print('*****************')
 """
 El nombre de la persona es: Raul
 La edad de la persona es: 22
 El apellido de la persona es: Gonzalez
 El sexo de la persona es: Masculino 
+"""
+# Instancio un empleado
+miEmpleado = Empleado('Maria', 22, 'Torres', 'Femenino')
+miEmpleado.datosPersonales()
+"""
+El nombre de la persona es: Raul
+Le edad de la persona es: 22
+El apellido de la persona es: Gonzalez
+El sexo de la persona es: Masculino
+"""
+print('*****************')
+miEmpleado.datosEmpleado(30, 80000)
+"""
+Sus dias de vacaciones son: 30
+Su salario es: 80000
 """
